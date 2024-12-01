3bc454ec420791cd8dfcc17c8269b528cbd32426
Sarah_Kerrigan <seregastrvs1@mail.ru>
2024-12-01 20:56:07 +0800

    fix errors

--- users/schemas.py
+++ users/schemas.py
@@ -2,7 +2,6 @@ from pydantic import BaseModel, EmailStr, Field
 from typing import Annotated
 from annotated_types import MaxLen, MinLen
 
-
 class CreateUser(BaseModel):
     username: Annotated[str, MinLen(3), MaxLen(20)]
     email: EmailStr
