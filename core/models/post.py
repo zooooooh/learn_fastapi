f82a6aef55d4dffe9ea583a34671e7f87e86b3d9
Sarah_Kerrigan <seregastrvs1@mail.ru>
2024-12-03 20:56:27 +0800

    create user relationship

--- core/models/post.py
+++ core/models/post.py
@@ -1,24 +1,21 @@
-from typing import TYPE_CHECKING
-from more_itertools.recipes import unique
-from sqlalchemy import String, Text, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column,relationship
+
+from sqlalchemy import String, Text
+from sqlalchemy.orm import Mapped, mapped_column
 
 
 from .base import Base
+from .mixins import UserRelationMixin
 
-if TYPE_CHECKING:
-    from .user import User
 
-class Post(Base):
 
+class Post(UserRelationMixin,Base):
+    # _user_id_unique: bool = False
+    # _user_id_nullable: bool = False
+    _user_back_populates = 'posts'
     title : Mapped[str] = mapped_column(String(100), unique=False)
     body: Mapped[str] = mapped_column(
         Text,
         default="",
         server_default="",
     )
-    user_id: Mapped[int] = mapped_column(
-        ForeignKey("users.id"),
-    )
-    user:Mapped["User"] = relationship(back_populates="posts")
-    
+
